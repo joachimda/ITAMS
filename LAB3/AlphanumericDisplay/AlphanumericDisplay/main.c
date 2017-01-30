529b8f572f975fb842075d457cb715622533e079
Joachim <joachimdamandersen@gmail.com>
2017-01-30 16:10:23 +0100

    Implemented sendData() function

---
+++ LAB3/AlphanumericDisplay/AlphanumericDisplay/main.c
@@ -0,0 +1,8 @@
+/*
+* main.c
+*
+* Created: 30-01-2017 13:42:53
+*  Author: Joachim
+*/
+
+#include <avr/io.h>
---
+++ LAB3a/GraphicDisplayLCD/GraphicDisplayLCD/main.c
@@ -0,0 +1,19 @@
+/*
+ * GraphicDisplayLCD.c
+ *
+ * Created: 30-01-2017 13:18:01
+ * Author : Joachim
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
