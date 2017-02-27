26932070432c989d0e7883314580bf5520f202c6
Joachim Dam Andersen <joachimdamandersen@gmail.com>
2017-02-27 20:34:15 +0100

    added new c++ project for GPS shield controller

---
+++ GPSDemo/GPSDemo/GPSDemo/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * GPSDemo.cpp
+ *
+ * Created: 27-02-2017 20:29:21
+ * Author : Joachim
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+#define F_CPU 3686400
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1)
+    {
+		
+    }
+}
+
