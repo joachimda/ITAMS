b3e102ca0de652508b25fb372de199b1c338bbf4
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-06 18:25:39 +0100

    started impl of gsm driver class

---
+++ final-project/final-project/GsmDriver.cpp
@@ -0,0 +1,19 @@
+/* 
+* GsmDriver.cpp
+*
+* Created: 06-03-2017 18:03:53
+* Author: Norgaard
+*/
+
+
+#include "GsmDriver.h"
+
+// default constructor
+GsmDriver::GsmDriver()
+{
+} //GsmDriver
+
+// default destructor
+GsmDriver::~GsmDriver()
+{
+} //~GsmDriver
---
+++ final-project/final-project/GsmDriver.h
@@ -0,0 +1,20 @@
+#ifndef __GSMDRIVER_H__
+#define __GSMDRIVER_H__
+
+class GsmDriver
+{
+	public:
+	GsmDriver();
+	~GsmDriver();
+	bool SendMessage(char* phonenumber, char* message);
+	char* GetMessage();
+	
+	private:
+	bool SetTextMode();
+	bool EnableEcho();
+	bool DisableEcho();
+	bool WaitForMessageReady();
+
+}; //GsmDriver
+
+#endif //__GSMDRIVER_H__
