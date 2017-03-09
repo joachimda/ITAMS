027dc97da80a7cfdb4b5f1bb7049c7391cbbd2d7
Joachim Dam Andersen <joachimdamandersen@gmail.com>
2017-03-09 19:11:20 +0100

    created new UART driver that uses internal interrupt for handling incoming messages

--- final-project-code/final-project/main.c
+++ final-project-code/final-project/main.c
@@ -1,11 +1,38 @@
 #define F_CPU 3686400
 #include <util/delay.h>
+#include <avr/io.h>
 #include "gsm/gsm.h"
+#include <avr/interrupt.h>
+
+#include "uart/uartwork.h"
+volatile unsigned int index = 0;
+
+#define USART_BAUDRATE 9600
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+
+unsigned char value;
+
+
+ISR(USART_RXC_vect){
+	
+	value = UDR;             //read UART register into value
+	//PORTB = ~value;          // output inverted value on LEDs (0=on)
+	sendString("1");
+}
 
 int main(void)
 {
-	gsmInit();
-	
+
+	sei();
+	//gsmInit();
+	for (int i = 0; i < DATA_SIZE;i++)
+	{
+		data[i] = 0;
+	}
+	UBRRL = BAUD_PRESCALE;
+	UBRRH = (BAUD_PRESCALE >> 8);
+	UCSRB = ((1<<TXEN)|(1<<RXEN) | (1<<RXCIE));
+	//initializeUART(9600,8);
 	unsigned char receivedChar = 0;
 	unsigned char index = 0;
 	unsigned char prefix[100] = {0};
@@ -15,4 +42,5 @@ int main(void)
 
 	unsigned char stopHere = 0;
 
+	while(1){}
 }
---
+++ final-project-code/final-project/uart/uartwork.c
@@ -0,0 +1,65 @@
+/*
+* uartDriver.c
+* Created: 20-02-2017 13:04:32
+*  Author: Joachim
+*/
+
+#include <avr/io.h>
+#include "uartwork.h"
+#include <stdlib.h>
+#define  F_CPU 3686400
+#include <util/delay.h>
+#define  XTAL 3686400
+
+void usartInit(unsigned char* data)
+{
+	for (int i = 0; i < DATA_SIZE;i++)
+	{
+		data[i] = 0;
+	}
+	UBRRL = BAUD_PRESCALE;
+	UBRRH = (BAUD_PRESCALE >> 8);
+	UCSRB = ((1<<TXEN)|(1<<RXEN) | (1<<RXCIE));
+}
+
+void sendString(unsigned char* s)
+{
+	// Repeat until zero-termination
+	while (*s != 0)
+	{
+		// Send the character pointed to by "s"
+		sendByte(*s);
+		// Advance the pointer one step
+		s++;
+	}
+}
+
+void sendByte(unsigned char u8Data)
+{
+	// Wait until last byte has been transmitted
+	while((UCSRA &(1<<UDRE)) == 0);
+
+	// Transmit data
+	UDR = u8Data;
+}
+
+void sendInteger(int num)
+{
+	char array[7];
+	// Convert the integer till an ASCII string (array), radix = 10
+	itoa(num, array, 10);
+	// - then send the string
+	sendString(array);
+}
+
+/*************************************************************************
+Flushed the RX buffer by toggling the receiver bit 4 in UCSRB
+Parameters:
+none
+*************************************************************************/
+void flushRxBuffer()
+{
+	UCSRB ^= (-0^UCSRB) & (1 << 4);
+	UCSRB ^= (-1^UCSRB) & (1 << 4);
+}
+
